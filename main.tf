a3696ea01203a1a898e96b61bf3c5d36b71c19e2
patrik tomantschger <patrik.tomantschger@arrow.com>
2021-08-04 08:59:05 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,36 +2,24 @@ provider "aws" {
 	region = "eu-central-1"
 	}
 
-variable "cidr_blocks" {
-	description = "vpc cidr blocks and name tags for vpc and subnets"
-	type = list(object({
-		cidr_block = string
-		name = string
-	}))
-	}
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
 
-resource "aws_vpc" "development-vpc" {
-	cidr_block = var.cidr_blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+	cidr_block = var.vpc_block
 	tags = {
-                Name = var.cidr_blocks[0].name
-                }
-	}	
-
-resource "aws_subnet" "dev-subnet-1" {
-	vpc_id = aws_vpc.development-vpc.id
-	cidr_block = var.cidr_blocks[1].cidr_block
-	availability_zone = "eu-central-1a"
-	tags = {
-		Name = var.cidr_blocks[1].name
+		Name = "${var.evn_prefix}-vpc"
 		}
 	}
 
-resource "aws_subnet" "dev-subnet-2" {
-        vpc_id = aws_vpc.development-vpc.id
-        cidr_block = var.cidr_blocks[2].cidr_block
-        availability_zone = "eu-central-1a"
-        tags = {
-                Name = var.cidr_blocks[2].name
-                }
+resource "aws_subnet" "myapp-subnet-1" {
+	vpc_id = aws_vpc.myapp-vpc.id	
+	cidr_block = var.subnet_cidr_block
+	availability_zone = var.avail_zone
+	tags = {
+		Name = "${var.evn_prefix}-subnet-1"
+		}
 	}
-
+	
