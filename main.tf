f5ce66d654d17be789e81f459fad17cd038533f3
patrik tomantschger <patrik.tomantschger@arrow.com>
2021-08-04 09:15:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-	region = "eu-central-1"
-	}
+        region = "eu-central-1"
+        }
 
 variable vpc_cidr_block {}
 variable subnet_cidr_block {}
@@ -8,18 +8,18 @@ variable avail_zone {}
 variable env_prefix {}
 
 resource "aws_vpc" "myapp-vpc" {
-	cidr_block = var.vpc_block
-	tags = {
-		Name = "${var.evn_prefix}-vpc"
-		}
-	}
+        cidr_block = var.vpc_cidr_block
+        tags = {
+                Name = "${var.env_prefix}-vpc"
+                }
+        }
 
 resource "aws_subnet" "myapp-subnet-1" {
-	vpc_id = aws_vpc.myapp-vpc.id	
-	cidr_block = var.subnet_cidr_block
-	availability_zone = var.avail_zone
-	tags = {
-		Name = "${var.evn_prefix}-subnet-1"
-		}
-	}
-	
+        vpc_id = aws_vpc.myapp-vpc.id
+        cidr_block = var.subnet_cidr_block
+        availability_zone = var.avail_zone
+        tags = {
+                Name = "${var.env_prefix}-subnet-1"
+                }
+        }
+
